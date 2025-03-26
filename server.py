e00029f840a70835f46ceb80f321d8724580ad63
Liav-W <liav.wintrob@gmail.com>
2025-03-26 11:12:18 +0200

    added domino sprites

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from random import randint
-
+import pygame
 
 class Card:
     def __init__(self, top, bottom):
@@ -54,7 +54,33 @@ class GameRoom ():
 
 def new_game ():
     hand = Hand([])  # Assuming an empty hand for the new players
-    players = [Player("Player1", hand), Player("Player2", hand), Player("Player3", hand)]
+    players = [Player("Player1", hand), Player("Player2", hand)]
     deck = Deck()  
     board = Board(deck)
     NewGame = GameRoom(board, players)
+
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
