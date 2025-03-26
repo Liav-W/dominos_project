a5113970f681287b806a543ebe51d9d1d7d8bb04
Liav-W <liav.wintrob@gmail.com>
2025-03-26 11:48:13 +0200

    added card display v1

--- server.py
+++ server.py
@@ -62,9 +62,17 @@ def new_game ():
 
 # pygame setup
 pygame.init()
+SCREEN_WIDTH, SCREEN_HEIGHT = 1280 , 720
 screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Image Click Event")
 clock = pygame.time.Clock()
 running = True
+
+image = pygame.transform.rotate(pygame.image.load("Dominoes\\1x\\0_0.png"), 90)
+image_rect = image.get_rect()
+
+image_rect.centerx = SCREEN_WIDTH // 2
+image_rect.bottom = SCREEN_HEIGHT
 
 while running:
     # poll for events
@@ -72,12 +80,15 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if image_rect.collidepoint(event.pos):
+                print("hello world")
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
     # RENDER YOUR GAME HERE
-
+    screen.blit(image, image_rect)
     # flip() the display to put your work on screen
     pygame.display.flip()
 
